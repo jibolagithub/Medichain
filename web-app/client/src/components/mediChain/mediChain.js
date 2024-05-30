5a09351277fbf796a5f490d24dee8be927d15e09
jibola <muhammedokunlola136@gmail.com>
2024-05-30 01:06:43 +0000

    Resolved merge conflict

---
+++ web-app/client/src/components/mediChain/mediChain.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AppAppBar from "../HomeFiles/AppAppBar";
+import ProductHero from "../HomeFiles/ProductHero";
+import TechStackInfo from "./techStackInfo";
+import ProjectInfo from "./projectInfo";
+import AppFooter from "../HomeFiles/AppFooter";
+
+export default function MediChain() {
+  console.log("here");
+  return (
+    <React.Fragment>
+      <AppAppBar />
+      <ProductHero />
+      <TechStackInfo />
+      <ProjectInfo />
+      <AppFooter />
+    </React.Fragment>
+  );
+}
